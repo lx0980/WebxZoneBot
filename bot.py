dd88cd26cefdc03562c7a596b7bc82e2d48b3e09
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-01 22:50:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from os import environ
 app_id = int(environ.get('API_ID'))
 api_hash = environ.get('API_HASH')
 bot_token = environ.get('BOT_TOKEN')
-chat_id = int(environ.get('TO_CHAT'))
+to_chat_id = int(environ.get('TO_CHAT'))
 from_chat_id = int(environ.get('FROM_CHAT'))
 
 webxzonebot = Client(    
@@ -14,18 +14,35 @@ webxzonebot = Client(
     bot_token=bot_token
 )
 
-@webxzonebot.on_message(filters.channel)
-async def forward(bot, message):
+@webxzonebot.on_message(filters.chat(from_chat_id))
+async def forward_media(_, message):
     try:
-        await bot.copy_message(
-            chat_id=chat_id,
-            from_chat_id=from_chat_id,
-            caption=f'**{message.caption}**',
-            message_id=message.id,
-            parse_mode=enums.ParseMode.MARKDOWN            
+        if message.chat.id == to_chat_id:
+            return
+
+        caption = message.caption or ""
+
+        if message.video:
+            media = message.video.file_id
+            media_type = "video"
+        elif message.document:
+            media = message.document.file_id
+            media_type = "document"
+        else:
+            return
+
+        await _.send_cached_media(
+            chat_id=to_chat_id,
+            file_id=media,
+            caption=f"**{caption}**" if caption else None,
+            parse_mode=enums.ParseMode.MARKDOWN
         )
+
+        print(f"✅ Forwarded {media_type} from {message.chat.id} → {to_chat_id}")
+
     except Exception as e:
-        print(f'{e}')
+        print(f"⚠️ Error while forwarding: {e}")
+        
 
 @webxzonebot.on_message(filters.command('start') & filters.user(5163706369))
 async def start(bot, message):
